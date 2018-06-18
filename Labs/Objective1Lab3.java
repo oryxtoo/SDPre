1c4a7c7c1f64569acde4be1bc97435b1e4fd939c
Stephanie <samcatee@gmail.com>
2018-06-17 18:19:45 -0600

    Objective 1

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String [] args) {
+    System.out.println("Name: Stephanie McAtee");
+    System.out.println("Age: 35");
+    System.out.println("State: Germany");
+  }
+}
