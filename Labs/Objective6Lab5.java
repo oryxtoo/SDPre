90090a244de8cc5ab0428ade2519fbcc10a52b5a
Stephanie <samcatee@gmail.com>
2018-06-18 17:39:18 -0600

    Objective 6

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Objective6Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while (true) { // while what is true? I need to print
+      //the menu out after printing the selection.
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+      if (selection == 1){
+        System.out.println("Hello World");
+      }
+      if (selection == 2){
+        System.out.println("Apple, Banana, Coconut");
+      }
+      else {
+        break;
+        System.out.println("Goodbye");
+      }
+    }
+    scanner.close();
+  }
+}
