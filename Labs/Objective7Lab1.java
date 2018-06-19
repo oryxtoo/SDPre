849ee5aeaebcda4b92fec2944ab0bb252a20ab0e
Stephanie <samcatee@gmail.com>
2018-06-19 08:54:42 -0600

    Objective 7

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,10 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+      Objective7Lab1 test = new Objective7Lab1();
+      test.printName();
+    }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
